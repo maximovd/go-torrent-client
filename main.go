051acf64e0ee09b6d01d74429b34187b57d5987f
Danil Maximov <jokjokjokerface@gmail.com>
2021-02-15 19:08:36 +0300

    Init app

---
+++ main.go
@@ -0,0 +1,22 @@
+package go_torrent_client
+
+import (
+	"go-torrent-client/torrentfile"
+	"log"
+	"os"
+)
+
+func main() {
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	tf, err := torrentfile.Open(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf.DownloadToFile(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
